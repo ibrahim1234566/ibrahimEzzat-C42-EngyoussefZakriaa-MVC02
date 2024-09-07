2725c9d7e2fa2043c11180816f66fb8335f28214
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-07 03:54:30 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ibrahimEzzat_C42_EngyoussefZakriaa_MVC02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
